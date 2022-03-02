1c4d59b759e100d559f33d82ba0b328814038c5e
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-03-02 21:29:54 +0100

    Initial commit

---
+++ emoji.js
@@ -0,0 +1,28 @@
+/**
+  {
+    "api":1,
+    "name":"Covert to slack emoji",
+    "description":"Convert text to slack emoji's",
+    "author":"Max",
+    "icon":"quote",
+    "tags":"emoji,slack,funny"
+  }
+**/
+
+function main(input) {
+    try {
+        input.text = input.text.split('').map(emoji).join('');
+    } catch (e) {
+        input.postError(e.message);
+    }
+}
+
+const regex = /^[0-9a-zA-Z]$/gm;
+
+function emoji(char) {
+    if (! char.match(regex)) {
+        return char;
+    }
+
+    return ':_' + char.toLowerCase() + ':';
+}
